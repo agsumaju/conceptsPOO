12ab3084c4a659667fafe248e3ad734eedc778f8
juan manuel suarez aguilar <agsumaju@gmail.com>
2023-10-20 16:25:44 -0500

    Finish Date Class

--- conceptsPOO/conceptsPOO/Date.cs
+++ conceptsPOO/conceptsPOO/Date.cs
@@ -16,17 +16,61 @@ namespace conceptsPOO
         {
             _year = year;
             _month = CheckMonth(month);
-            _day = day;
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2  && month == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+            int[] daysPerMonth = { 0, 31, 28, 31, 30,31,30,31,31,30,31,30,31 };
+
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+            throw new DayException("Invalid day");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 == 0)
+            //    {
+            //        if (year % 400 == 0)
+            //        {  
+            //            return true; 
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return true;
+            //    }
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            throw new NotImplementedException();
         }
 
         private int CheckMonth(int month)
         {
-            if(month >=1 && month >= 12) 
+            if (month >= 1 && month <= 12) 
             {
                 return month;
             }
 
-            throw new MonthException("Onvalid Month");
+            throw new MonthException("Invalid Month");
         }
 
         public override string ToString()
--- conceptsPOO/conceptsPOO/Program.cs
+++ conceptsPOO/conceptsPOO/Program.cs
@@ -5,7 +5,14 @@ Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
 
 
-Console.WriteLine(new Date(2014,07,02));
-Console.WriteLine(new Date(2018, 45, 02));
-Console.WriteLine(new Date(2023, 12, 45));
-Console.WriteLine(new Date(2017, 07, 02));
+try
+{
+	Console.WriteLine(new Date(2022, 12, 7));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(2023, 10, 02));
+	Console.WriteLine(new Date(2017, 07, 31));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
