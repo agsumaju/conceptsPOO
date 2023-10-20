d8b2c23473a01daed9e3187613c68648999a119d
juan manuel suarez aguilar <agsumaju@gmail.com>
2023-10-20 11:07:04 -0500

    Update Class#

---
+++ conceptsPOO/conceptsPOO/Date.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conceptsPOO
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = day;
+        }
+
+        private int CheckMonth(int month)
+        {
+            if(month >=1 && month >= 12) 
+            {
+                return month;
+            }
+
+            throw new MonthException("Onvalid Month");
+        }
+
+        public override string ToString()
+        {
+            return $"{ _year}/{ _month:00}/{_day:00}";
+        }
+    }
+}
---
+++ conceptsPOO/conceptsPOO/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using conceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+
+Console.WriteLine(new Date(2014,07,02));
+Console.WriteLine(new Date(2018, 45, 02));
+Console.WriteLine(new Date(2023, 12, 45));
+Console.WriteLine(new Date(2017, 07, 02));
